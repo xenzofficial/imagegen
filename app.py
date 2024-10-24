8e2ab10c695260b793efb3b3e47c1740ec03ec3d
Call Me AL <116076493+xenzofficial@users.noreply.github.com>
2024-10-25 03:48:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -195,6 +195,15 @@ HTML_TEMPLATE = '''
             height: 100%;
             width: 100%;
         }
+	video {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+	    z-index: -1
+        }
         h1 {
             font-family: 'New Rocker', cursive;
             color: #000;
